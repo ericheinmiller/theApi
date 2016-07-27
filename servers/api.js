085d09143e1e35b4fff9fffe04c5ef5de0c6a78e
ericheinmiller <ericheinmiller@gmail.com>
2016-07-26 23:26:43 -0700

    refactored code in api.js to make it more readable

--- servers/api.js
+++ servers/api.js
@@ -6,16 +6,17 @@ var api = function(req, res){
     res.json(false);
     return;
   }
+
   //define variables
-  var theObject = {
+  var hashTable = {
     '}' : '{',
     ']' : '[',  //the role of theObject is to keep key value pairs of each parentheses.
     ')' : '('
   },
-  i = 0, //define starting pointer
-  str = req.body.str, //the inputted string  
-  length = str.length,  //length of string inputted
-  theArray = []; //test array
+  i = 0,                            //define starting pointer
+  submittedString = req.body.str,   //the inputted string  
+  length = submittedString.length,  //length of string inputted
+  stackArray = [];                  //test array
 
   //if the string is an odd length, return false
   if(length % 2 !== 0){
@@ -25,38 +26,35 @@ var api = function(req, res){
 
   //loop through the string;
   for(i ; i <= length -1 ; i++){
-
+    var indexValue = submittedString[i],
+    lastIndex = stackArray.length-1;
     //if the character in the string is one of theObjects values, push it into theArray
-    if(str[i] === '{' || str[i] === '[' || str[i] === '('){
-      theArray.push(str[i]);
+    if( 
+       indexValue === '{' ||
+       indexValue === '[' ||
+       indexValue === '(' ){
+      stackArray.push(stringValue);
     }
-
     //using the object we check to see if value of the last index of the array matches the object's key value pair. If not, we return false and stop the app
     else{
-
-      //if the array is empty when the else statement is called, it means that there wasn't an ititial push of an opening parentheses
-      if(theArray.length === 0){
+      if(stackArray.length === 0){  //if the array is empty when the else statement is called, 
+        res.json(false);            //it means that there wasn't an ititial push of an opening parentheses
+        return
+      }
+      else if(stackArray[lastIndex] !== hashTable[indexValue]){    //if they don't match, return false
         res.json(false);
         return
       }
-
-      //if they don't match, return false
-      if(theArray[theArray.length -1] !== theObject[str[i]]){
-        res.json(false);
-        return
-      }
-
-      //if they match, we remove the last index from the array as they are matching pairs.
       else{
-        if(theObject[str[i]] === theArray[theArray.length-1]){
-          theArray.pop();
+        if(hashTable[indexValue] === stackArray[lastIndex]){   //if they match, we remove the last 
+          stackArray.pop();                                     //index from the array as they are matching pairs.
         }
       }
     }
   }
 
   //if the array is not empty, that means there are pairs that never found a match.
-  if(theArray.length !== 0){
+  if(stackArray.length !== 0){
     res.json(false);
     return;
   }
